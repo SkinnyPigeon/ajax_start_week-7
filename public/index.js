1afccbd3df407710f43566244b30ba6b418aaab1
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-19 22:29:34 +0100

    cannot get an if statement to work in order to sort the regions out

--- public/index.js
+++ public/index.js
@@ -15,21 +15,36 @@ window.onload = function() {
   request.send( null );
 }
 
+
 function main() {
 
-  countries.forEach( function( country ) {
-    var option = document.createElement( "option" );
-    option.innerHTML = country.region;
-    var select = document.getElementById( "region-selector" );
-    select.appendChild( option )
+  var regions = [ "Asia", "Africa", "Americas", "Europe", "Oceania" ]
+
+  regions.forEach( function( country ) {
+    var optionRegion = document.createElement( "option" );
+    optionRegion.innerHTML = country;
+    var selectRegion = document.getElementById( "region-selector" );
+    selectRegion.appendChild( optionRegion )
   })
 
+  addedRegion = JSON.parse( localStorage.getItem( "region_list" ) ) || [];
+
+
+  var regionButton = document.getElementById( "region-select" );
+  var regionForm = document.getElementById( "region" );
+  regionButton.onclick = function( event ) {
+    event.preventDefault( event );
+    var currentRegion = document.getElementById( "region-selector" ).value;
+  }
+
   countries.forEach( function( country ) {
-    var option = document.createElement( "option" );
+    // if( country.region === currentRegion )
+      var option = document.createElement( "option" );
     option.innerHTML = country.name;
     var select = document.getElementById( "selector" );
     select.appendChild( option );
   } )
+
 
   addedCountry = JSON.parse( localStorage.getItem( "country_app_list" ) ) || [];
   createEntry( addedCountry )
@@ -50,55 +65,55 @@ function main() {
   }
 
   function createEntry( found ){
-     var ul = document.getElementById('country-list');
-     if(ul.childNodes[0]){
-       ul.removeChild(ul.childNodes[0]);
-     }
-     countryCard( ul, found )
+   var ul = document.getElementById('country-list');
+   if(ul.childNodes[0]){
+     ul.removeChild(ul.childNodes[0]);
    }
+   countryCard( ul, found )
+ }
 
-   function countryCard( ul, found ) {
+ function countryCard( ul, found ) {
 
-    var li = document.createElement('li');
-    var hr = document.createElement('hr');
-    var he = document.createElement( "h2" );
+  var li = document.createElement('li');
+  var hr = document.createElement('hr');
+  var he = document.createElement( "h2" );
 
-    li.innerHTML =  "<h3>Name:</h3> " + found.name + " <h3>Capital:</h3> " + found.capital + " <h3>Pop:</h3> " + found.population
-    he.innerHTML = "Neighbours:"
+  li.innerHTML =  "<h3>Name:</h3> " + found.name + " <h3>Capital:</h3> " + found.capital + " <h3>Pop:</h3> " + found.population
+  he.innerHTML = "Neighbours:"
 
-    ul.appendChild(li);
-    li.appendChild(hr);
-    li.appendChild( he );
+  ul.appendChild(li);
+  li.appendChild(hr);
+  li.appendChild( he );
 
-    localStorage.setItem( "country_app_list" , JSON.stringify( found ) );
-    borderCountries( found.borders )
-   }
+  localStorage.setItem( "country_app_list" , JSON.stringify( found ) );
+  borderCountries( found.borders )
+}
 
-   function borderCountries( countryBorders ) {
-    var borders = [];
-    for( border of countryBorders ) {
-      var neighbour = countries.filter( function( country ) {
-        return country.alpha3Code === border;
-      });
-      borders.push( neighbour );
-    } 
-    borderCheck( borders )
-   }
+function borderCountries( countryBorders ) {
+  var borders = [];
+  for( border of countryBorders ) {
+    var neighbour = countries.filter( function( country ) {
+      return country.alpha3Code === border;
+    });
+    borders.push( neighbour );
+  } 
+  borderCheck( borders )
+}
 
-   function borderCheck( borders ) {
-    var borderList = document.getElementById( "border-list" )
-    while (borderList.childNodes[0] != null ) {
-      borderList.removeChild( borderList.firstChild );
-    }
+function borderCheck( borders ) {
+  var borderList = document.getElementById( "border-list" )
+  while (borderList.childNodes[0] != null ) {
+    borderList.removeChild( borderList.firstChild );
+  }
 
-    for( country of borders) {
-      var li = document.createElement( "li" );
-      var hr = document.createElement( "hr" );
-      li.innerHTML = "<h4>Name:</h4>" + country[0].name + "<h4>Capital:</h4>" + country[0].capital + "<h4>Pop: </h4>" + country[0].population;
-      borderList.appendChild( li );
-      borderList.appendChild( hr );
-    }
-   }
+  for( country of borders) {
+    var li = document.createElement( "li" );
+    var hr = document.createElement( "hr" );
+    li.innerHTML = "<h4>Name:</h4>" + country[0].name + "<h4>Capital:</h4>" + country[0].capital + "<h4>Pop: </h4>" + country[0].population;
+    borderList.appendChild( li );
+    borderList.appendChild( hr );
+  }
+}
 
 }
 
